7985a3d469f480754b398c7781a149a8ff5a4204
Lee Soojin <76468868+Lee-Soojin@users.noreply.github.com>
2025-03-09 15:39:27 +0900

    feat: 초기 설정

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import Router from "./router/stock.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("tiny"));
+
+app.use("/stock", Router);
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.sendStatus(500);
+});
+
+app.listen(8080);
